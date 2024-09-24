7780b3557e3a9c53cb7f209b1258c722ba1f29a5
pavlo-m-ai <pavlo.v.martyniuk@ukd.edu.ua>
2024-09-24 13:20:15 +0300

    Motosale

---
+++ test.js
@@ -0,0 +1,6 @@
+const motoname = "KTM sx-f 450";
+const motoyear = 2024;
+let motoprice = 10900;
+let availabel = true;
+let buyername = null;
+console.log (`Назва: ${motoname}, Рік: ${motoyear}, Ціна: ${motoprice} Євро, Доступ: ${availabel}, Покупець: ${buyername}`)
